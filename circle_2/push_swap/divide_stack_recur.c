ad3a2b56d86f5af30080c28053691a8aab4cd4c2
sweetykr7 <sweetykr@naver.com>
2022-07-06 17:49:18 +0900

    220706

--- circle_2/push_swap/divide_stack_recur.c
+++ circle_2/push_swap/divide_stack_recur.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   divide_stack_recur.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joey <joey@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: sooyokim <sooyokim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 11:48:52 by sooyokim          #+#    #+#             */
-<<<<<<< HEAD
 /*   Updated: 2022/07/06 16:00:08 by sooyokim         ###   ########.fr       */
-=======
-/*   Updated: 2022/07/05 18:43:58 by joey             ###   ########.fr       */
->>>>>>> 5bacde3b30f7f8306311c73242efe815a428b1b9
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,40 +67,6 @@ void	divide_stack_a_check(t_head *a, t_head *b, t_pivot pivot, t_buf *buf)
 	}
 }
 
-int	find_zero_data(t_head *a, t_head *b)
-{
-	t_list	*temp;
-	int			i;
-
-	temp = a->head;
-	i = 0;
-	while (i < a->total_cnt)
-	{
-		if (temp->data == 0)
-			return (-1);
-		i++;
-		if (temp->next)
-			temp = temp->next;
-		else
-			break;
-	}
-
-	temp = b->head;
-	i = 0;
-	while (i < b->total_cnt)
-	{
-		if (temp->data == 0)
-			return (-2);
-		i++;
-		if (temp->next)
-			temp = temp->next;
-		else
-			break;
-	}
-
-	return (1);
-}
-
 void	divide_stack_b_check(t_head *a, t_head *b, t_pivot pivot, t_buf *buf)
 {
 	if (b->head->cluster_cnt <= 3)
@@ -123,7 +85,6 @@ void	divide_stack_recur(t_head *a, t_head *b, int total_cnt, t_buf *buf)
 {
 	t_pivot	pivot;
 	int		check_sort_a_res;
-<<<<<<< HEAD
 	// int		check_data_zero_res;
 
 	// check_data_zero_res = check_data_zero(a, b);
@@ -151,14 +112,6 @@ void	divide_stack_recur(t_head *a, t_head *b, int total_cnt, t_buf *buf)
 	// 	}
 	// 	exit (0);
 	// }
-=======
-	int		test_point;
-
-	if (find_zero_data(a, b) == -1)
-		test_point = 0; //a쪽
-	if (find_zero_data(a, b) == -2)
-		test_point = 0; //b쪽
->>>>>>> 5bacde3b30f7f8306311c73242efe815a428b1b9
 	pivot.pivot1 = 0;
 	pivot.pivot2 = 0;
 	check_sort_a_res = 0;
--- circle_2/push_swap/ref/util_stack_b.c
+++ circle_2/push_swap/ref/util_stack_b.c
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   util_stack_b.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joey <joey@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: sooyokim <sooyokim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 14:29:04 by sooyokim          #+#    #+#             */
-<<<<<<< HEAD:circle_2/push_swap/ref/util_stack_b.c
 /*   Updated: 2022/07/06 10:35:27 by sooyokim         ###   ########.fr       */
-=======
-/*   Updated: 2022/07/05 20:50:46 by joey             ###   ########.fr       */
->>>>>>> 5bacde3b30f7f8306311c73242efe815a428b1b9:circle_2/push_swap/util_stack_b.c
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +23,7 @@ void	pb(t_head *a, t_head *b, t_buf *print_buf)
 	push(b, pull_data);
 	//b->head->cluster_cnt = cluster_cnt;
 	insert_print_buf(print_buf, "pb");
-<<<<<<< HEAD:circle_2/push_swap/ref/util_stack_b.c
 	printf("pass pb!\n");
-=======
-	printf("pb\n");
->>>>>>> 5bacde3b30f7f8306311c73242efe815a428b1b9:circle_2/push_swap/util_stack_b.c
 }
 
 void	sb(t_head *b, t_buf *print_buf)
@@ -57,11 +49,7 @@ void	sb(t_head *b, t_buf *print_buf)
 	push(b, pull_data_2);
 	b->head->cluster_cnt = cluster_cnt2;
 	insert_print_buf(print_buf, "sb");
-<<<<<<< HEAD:circle_2/push_swap/ref/util_stack_b.c
 	printf("pass sb!\n");
-=======
-	printf("sb\n");
->>>>>>> 5bacde3b30f7f8306311c73242efe815a428b1b9:circle_2/push_swap/util_stack_b.c
 }
 
 void	rrb(t_head *b, int sort_check, t_buf *print_buf)
@@ -75,12 +63,7 @@ void	rrb(t_head *b, int sort_check, t_buf *print_buf)
 	if (b->total_cnt == 1)
 		return ;
 	if (b->total_cnt == 2)
-<<<<<<< HEAD:circle_2/push_swap/ref/util_stack_b.c
 		return (sb(b, print_buf));
-=======
-		return sb(b, print_buf);
-	temp = b->head;
->>>>>>> 5bacde3b30f7f8306311c73242efe815a428b1b9:circle_2/push_swap/util_stack_b.c
 	while (temp->next)
 	{
 		if (!(temp->next->next))
@@ -96,16 +79,11 @@ void	rrb(t_head *b, int sort_check, t_buf *print_buf)
 		}
 		temp = temp->next;
 	}
-	printf("check rrb's pull_data : %d",pull_data);
 	push(b, pull_data);
 	if (sort_check != 0)
 		b->head->cluster_cnt = sort_check;
 	insert_print_buf(print_buf, "rrb");
-<<<<<<< HEAD:circle_2/push_swap/ref/util_stack_b.c
 	printf("pass rrb!\n");
-=======
-	printf("rrb\n");
->>>>>>> 5bacde3b30f7f8306311c73242efe815a428b1b9:circle_2/push_swap/util_stack_b.c
 }
 
 void	rb(t_head *b, t_buf *print_buf)
@@ -116,17 +94,11 @@ void	rb(t_head *b, t_buf *print_buf)
 	pull_data = 0;
 	if (b->total_cnt == 1)
 		return ;
-	if (b->total_cnt == 2)
-		return sb(b, print_buf);
 	sort_check = 0;
 	if (b->head->cluster_cnt == -1)
 		sort_check = -1;
 	pull_data = pull(b);
 	list_add(b, pull_data, sort_check);
 	insert_print_buf(print_buf, "rb");
-<<<<<<< HEAD:circle_2/push_swap/ref/util_stack_b.c
 	printf("pass rb!\n");
-=======
-	printf("rb\n");
->>>>>>> 5bacde3b30f7f8306311c73242efe815a428b1b9:circle_2/push_swap/util_stack_b.c
 }
